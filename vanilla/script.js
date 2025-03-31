4eb1579213ac1492a449c0ac1de4f5fc5512222e
Arjun Shenoy <arjunshenoy23@gmail.com>
2025-03-31 13:07:39 +0530

    Refactored Code

---
+++ vanilla/script.js
@@ -0,0 +1,20 @@
+//Add Pages Checkbox Functionality (Extras)
+document.addEventListener("DOMContentLoaded", function () {
+    const selectAllCheckbox = document.querySelector(".allPages"); // "All Pages" checkbox
+    const pageCheckboxes = document.querySelectorAll(".Pages"); // All individual checkboxes
+    // When "All Pages" checkbox is clicked
+    selectAllCheckbox.addEventListener("change", function () {
+        pageCheckboxes.forEach(checkbox => {
+            checkbox.checked = selectAllCheckbox.checked;
+        });
+    });
+    // When any individual checkbox is clicked
+    pageCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener("change", function () {
+            // If all checkboxes are checked, check "All Pages"
+            // If any is unchecked, uncheck "All Pages"
+            selectAllCheckbox.checked = [...pageCheckboxes].every(cb => cb.checked);
+        });
+    });
+});
+
